aa8bb9c550d5cd8fea037662ddbe0ac0996878a7
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-08-20 21:38:59 +0900

    feat: train callback hamda async functions

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-console.log("Jack Ma maslahatlari");
+console.log("Jack Ma maslahatlari");                              // CallBack function
 const list = [
     "yaxshi talaba bo'ling",  // 0-20
     "togri boshliq tanlang va koproq hato qiling", // 20-30
@@ -8,6 +8,7 @@ const list = [
     "endi dam oling, foydasi yoq endi", // 60
 ]
 
+//  CALLBACK functions
 function maslahatBering(a, callback) {
     if(typeof a !== 'number') callback("insert a number", null);
     else if(a <= 20) callback(null, list[0]); 
@@ -16,9 +17,10 @@ function maslahatBering(a, callback) {
     else if(a > 40 && a <= 50) callback(null, list[3]);
     else if(a > 50 && a <= 60) callback(null, list[4]);
     else {
-        setTimeout(function() {
+        // setTimeout(function() {
+          setInterval(function () {
             callback(null, list[5]);
-        }, 5000);
+        }, 1000);
     }
 }
 
@@ -30,3 +32,62 @@ maslahatBering( 65, (err, data) => {            // agar son orniga sozlar yozils
     }
 });
 console.log("passed here 1");
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");                              // Async functionlari
+// const list = [
+//     "yaxshi talaba bo'ling",  // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60
+// ]
+
+
+// ASYNC functions
+// async function maslahatBering(a) {
+//     if(typeof a !== 'number') throw new Error("insert a number");
+//     else if(a <= 20) return list[0]; 
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list [5]); 
+//             }, 5000);
+//         });
+//         // setTimeout(function() {
+//         //     return list[5];
+//         // }, 5000);
+//     }
+// }
+
+
+//  call via then / catch
+// console.log('passed here 0');
+// maslahatBering(25)
+//    .then(data =>{
+//     console.log('javob:', data);
+// })
+//   .catch(err => {
+//     console.log("ERROR:", err);
+// })
+// console.log('passed here 1');
+
+
+//  call via  async / await                                   // hammasini ketma ket chaqirish
+// async function run() {
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
