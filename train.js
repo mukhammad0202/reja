80b102220d368acfc1b5ca386b370c54ace03eeb
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-08-21 14:41:03 +0900

    feat: completed a-task page

--- train.js
+++ train.js
@@ -1,37 +1,37 @@
-console.log("Jack Ma maslahatlari");                              // CallBack function
-const list = [
-    "yaxshi talaba bo'ling",  // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashingizni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling, foydasi yoq endi", // 60
-]
+// console.log("Jack Ma maslahatlari");                              // CallBack function
+// const list = [
+//     "yaxshi talaba bo'ling",  // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60
+// ]
 
-//  CALLBACK functions
-function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback("insert a number", null);
-    else if(a <= 20) callback(null, list[0]); 
-    else if(a > 20 && a <= 30) callback(null, list[1]);
-    else if(a > 30 && a <= 40) callback(null, list[2]);
-    else if(a > 40 && a <= 50) callback(null, list[3]);
-    else if(a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        // setTimeout(function() {
-          setInterval(function () {
-            callback(null, list[5]);
-        }, 1000);
-    }
-}
+// //  CALLBACK functions
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("insert a number", null);
+//     else if(a <= 20) callback(null, list[0]); 
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 40) callback(null, list[2]);
+//     else if(a > 40 && a <= 50) callback(null, list[3]);
+//     else if(a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         // setTimeout(function() {
+//           setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+// }
 
-console.log('passed here 0');
-maslahatBering( 65, (err, data) => {            // agar son orniga sozlar yozilsa "salom" error chiqaradi
-    if(err) console.log ('ERROR', err);
-    else {
-        console.log('Javob:', data);
-    }
-});
-console.log("passed here 1");
+// console.log('passed here 0');
+// maslahatBering( 65, (err, data) => {            // agar son orniga sozlar yozilsa "salom" error chiqaradi
+//     if(err) console.log ('ERROR', err);
+//     else {
+//         console.log('Javob:', data);
+//     }
+// });
+// console.log("passed here 1");
 
 
 
@@ -91,3 +91,22 @@ console.log("passed here 1");
 //     console.log(javob);
 // }
 // run();
+
+
+// =======================================     A-TASK       =================================
+
+
+
+
+function countLetter(letter, word) {
+        let count = 0;
+    for( let i = 0; i <= word.length; i++) {
+        if(word[i] === letter) {
+            count++
+        }
+    }
+    return count;
+}
+const result = countLetter("e", "engineer");
+console.log(result);
+
