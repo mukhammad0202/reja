dd446026350f583b3a3873d339a386b5ce942e3e
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-01 20:38:02 +0900

    feat: completed port page

--- server.js
+++ server.js
@@ -2,24 +2,29 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://paulo:869ENO8ZHpNkXmJE@cluster0.ubvmg.mongodb.net/Reja"
+const connectionString =
+  "mongodb+srv://paulo:869ENO8ZHpNkXmJE@cluster0.ubvmg.mongodb.net/Reja";
 
-mongodb.connect(connectionString, {
+mongodb.connect(
+  connectionString,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err, client) => {
-    if(err) console.log("ERROR on connection MongoDB");
-    else{
-        console.log("MongoDB connection succeed");
-        module.exports = client;
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
 
-        const app = require("./app");
-        const server = http.createServer(app);
-        let PORT = 3000;
-        server.listen(PORT, function () {
-        console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-    });      
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 6007;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
     }
-}); 
-
-
+  }
+);
